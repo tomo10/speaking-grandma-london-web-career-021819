c739b4701c610a86f5eaeb264fd4b160a57331df
Thomas Edwards <thce28@gmail.com>
2019-02-08 13:28:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,12 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
   
-  elsif string.downcase == string 
-  return "HUH?! SPEAK UP, SONNY!"
-
   elsif string.upcase == string 
   return "NO, NOT SINCE 1938!"
+  
+  else string.downcase == string 
+  return "HUH?! SPEAK UP, SONNY!"
+
   
   
 end
